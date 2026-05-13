75e2941cd3401c34e8bf70a285b95c295dc352f6
Juan David Escobar <31302412+juanec6@users.noreply.github.com>
2026-05-12 23:08:58 -0500

    Include Secant method in numerical comparison program
    Added Secant method implementation to main program.

--- Codes/main.cpp
+++ Codes/main.cpp
@@ -11,6 +11,7 @@
 #include <thread>
 #include "Bisection.h"
 #include "NewtonRaphKantarovich.h"
+#include "Secant.h"
 #include "Functions.h"
 
 using namespace std;
@@ -104,9 +105,16 @@ int main()
 	cout << endl;
 	bisectionMethod(xi, xf, step, tol, option);
 	cout << endl;
+	// SECANT
+	cout << endl;
+	cout << "Raices encontradas para el Método de la Secante (temporal)" << endl;
+	cout << endl;
+	secantMethod(xi, xf, step, tol, option);
+	cout << endl;
 	cout << "Press enter to finish";
 	cin.ignore(); // Limpia el buffer del último Enter
 	cin.get();    // Espera un Enter
+
 
     return 0;
 }
