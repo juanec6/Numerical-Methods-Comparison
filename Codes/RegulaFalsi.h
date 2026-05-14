9e64a86bd8f73876ba89c2faf414c56f9ee9ca5b
Juan David Escobar <31302412+juanec6@users.noreply.github.com>
2026-05-13 20:32:39 -0500

    Implement Regula Falsi method in RegulaFalsi.h
    Added Regula Falsi method implementation for root finding.

--- Codes/RegulaFalsi.h
+++ Codes/RegulaFalsi.h
@@ -1,18 +1,51 @@
-#ifndef BISECTION_H
-#define BISECTION_H
+#ifndef REGULAFALSI_H
+#define REGULAFALSI_H
 #include <cmath>
 #include <string>
-#include "functions.h" // Here are the f(x, opcion) value. 
+#include <vector>
+#include "Functions.h" // Here are the f(x, opcion) value. 
 using namespace std;
 
-double bisectionM(double xi, double xf, double step, double tol, int option)
+double regulaFalsiMethod(double xi, double xf, double step, double tol, int option)
 {
-	for(int i = xi; i <= xf; i += step)
+	// Variables necesarias.
+	double xr, x0, x1;
+	int iter = 0, maxIter = 150;
+	vector<double> raices, xValues, yValues;
+	
+	// Regula falsi implementation
+	for(double i = xi; i <=xf; i = i + step)
 	{
-		cout<<f(i, 1)<<endl;
+		x0 = i;
+		x1 = i + step;
+		if(f(x0, option)*f(x1, option) < 0)
+		{
+			do
+			{
+				xr = x1 - (f(x1, option)*(x1 - x0))/(f(x1, option) - f(x0, option));
+				if(f(x0, option)*f(xr, option) < 0)
+				{
+					x1 = xr;
+				} else {
+					x0 = xr;
+				}
+				iter++;
+			} while(abs(x1 - x0) > tol && iter < maxIter);
+			
+			// Se redondea la raíz a cero si:
+			if(abs(xr) < 1e-10)
+			{
+				xr = 0;
+			}
+			raices.push_back(xr);
+		}
 	}
-	cout<<tol;
-	cout<<option;
+	
+	// Imprime las raíces en main
+	for(int i = 0; i < raices.size(); i++)
+	{
+		std:cout << "Raíz " << i + 1 << ": " << raices[i] << std::endl;
+	}
 	return 0;
 }
 
